f0ec73873974746368ee10ffdc862670486593c3
Dogmin <kdm3578@gmail.com>
2021-11-20 12:34:24 +0900

    update

--- binary_search/정렬된 배열에서 특정 수의 개수 구하기.py
+++ binary_search/정렬된 배열에서 특정 수의 개수 구하기.py
@@ -1,17 +1,21 @@
 from bisect import bisect_left,bisect_right
 
-n,x = map(int, input().split())
 
-num = list(map(int, input().split()))
+# 정렬된 리스트에서 값이 특정 범위에 속하는 원소의 개수를 구하는데 효과적
+# 값이 [lef_value, right_value]인 데이터의 개수를 반환하는 함수
 
-def count_by_range(a, left_value, right_value):
-	right_index = bisect_right(a,right_value)
-	left_index = bisect_left(a,left_value)
-	if right_index - left_index == 0:
-		return -1
-	else:
-		return right_index-left_index
-	
+def count_by_range(a, lef_value, right_value):
+	right_index = bisect_right(a, right_value)
+	left_index = bisect_left(a, lef_value)
+	return right_index - left_index
 
-print(count_by_range(num,x,x))
+
+# 리스트 선언
+a = [1, 2, 3, 3, 3, 3, 4, 4, 8, 9]
+
+# 값이 4인 데이터 개수 출력
+print(count_by_range(a, 4, 4))
+
+# 값이 [-1,3] 범위에 있는 데이터 개수 출력
+print(count_by_range(a, -1, 3))
 
---
+++ 기타 알고리즘/소수판별.py
@@ -0,0 +1,28 @@
+import math
+
+
+#수 하나의 소수 판별
+def is_prime_number(x):
+    #2부터 x의 제곱근까지의 모든 수를 확인하며
+    for i in range(2, int(math.sqrt(x)) + 1):
+        #x가 해당 수로 나누어떨어진다면
+        if x % i == 0:
+            return False #소수가 아님
+    return True
+
+#여러개의 수가 소수인지 아닌지를 판별 - 에라토스테네스의 체
+#N보다 작거나 같은 모든 소수를 찾을 때 사용 가능
+
+def eratostenes(n):
+    array = [True for i in range(n+1)] # 처음엔 모든 수가 소수(True)인 것으로 초기화(0,1은 제외)
+
+    for i in range(2, int(math.sqrt(n)) + 1):  # 2부터 n의 제곱근까지의 모든 수를 판별
+        if array[i] == True:  # i가 소수인 경우(남은 수인 경우)
+            # i를 제외한 모든 배수를 지우기
+            j = 2
+            while i * j <= n:
+                array[i * j] = False
+                j += 1
+    for i in range(2, n + 1):  # 모든 소수 출력
+        if array[i]:
+            print(i, end=' ')
