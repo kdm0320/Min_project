68d5df155a6f86e85e5cc623152bb63b2660f21d
Dogmin <kdm3578@gmail.com>
2021-12-03 00:47:39 +0900

    update

--- 기타 라이브러리/collections_library.py
+++ 기타 라이브러리/collections_library.py
@@ -1,11 +1,11 @@
-#deque
-#큐를 구현할때 사용
-#deque 자료형은 리스트와 달리 인슬싱, 슬라이싱 사용 불가
-#연속적으로 나열된 데이터의 시작부분이나 끝부분에 데이터를 삽입하거나 삭제할때 효과적
-#스택이나 큐의 대용으로 사용가능
-#첫번째 원소 제거할때 popleft(), 끝 원소 제거할떄 pop()
-#첫번째 인덱스에 원소 x를 삽입할때 appendleft(x), 마지막 인덱스에 삽일할때 append(x)
-#deque를 큐로 이용할때 원소 삽입할때 append(),삭제할때는 poplef()
+# deque
+# 큐를 구현할때 사용
+# deque 자료형은 리스트와 달리 인슬싱, 슬라이싱 사용 불가
+# 연속적으로 나열된 데이터의 시작부분이나 끝부분에 데이터를 삽입하거나 삭제할때 효과적
+# 스택이나 큐의 대용으로 사용가능
+# 첫번째 원소 제거할때 popleft(), 끝 원소 제거할떄 pop()
+# 첫번째 인덱스에 원소 x를 삽입할때 appendleft(x), 마지막 인덱스에 삽일할때 append(x)
+# deque를 큐로 이용할때 원소 삽입할때 append(),삭제할때는 poplef()
 
 from collections import deque,Counter
 
@@ -16,11 +16,18 @@ data.append(5)
 print(data)
 print(list(data))
 
-#Counter = Iterable한 객체 내부의 원소가 몇번씩 등장했는지 알려준다
+# Counter = Iterable한 객체 내부의 원소가 몇번씩 등장했는지 알려준다
 
 counter = Counter(['red','blue','green','blue','blue'])
 print(counter['blue'])
 print(counter['green'])
 print(counter)
+# most_common = 데이터의 개수가 많은 순으로 정렬된 배열을 리턴
 
-#
+counter_most_common = Counter('hello world').most_common()
+print(counter_most_common)
+
+# 이 메서드의 인자로 숫자 K를 넘기면 그 숫자 만큼만 리턴하기 때문에,
+# 가장 개수가 많은 K개의 데이터를 얻는것도 가능
+max_counter_most_common = Counter('hello world').most_common(1)
+print(max_counter_most_common)
