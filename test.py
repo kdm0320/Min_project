11bde29e128c2bbb44b0f44b6e5edb77e1fcd00e
Dogmin <kdm3578@gmail.com>
2021-12-28 22:36:07 +0900

    update

--- test.py
+++ test.py
@@ -1,7 +1,12 @@
+def a(i):
+    if i==5:
+        print("check_finish")
+        return
+    print(f"{i} 번째에서 {i+1} 번째 함수를 호출합니다")
+    print("check")
+    a(i+1)
+    print("check1")
+    print(i, "번째 재귀함수를 종료합니다.")
 
 
-
-check = {1:1,2:0,3:0}
-temp =  [i for i in check.values() ]
-
-print(temp)
+a(1)
