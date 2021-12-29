5439b420343da281b606836e17b98b6bfd8e6fbf
Dongmin <kdm3578@gmail.com>
2021-12-29 20:15:26 +0900

    update

---
+++ 코딩테스트 연습/약수의 개수와 덧셈.py
@@ -0,0 +1,16 @@
+from math import sqrt
+
+
+def solution(left, right):
+    answer = 0
+    for i in range(left,right+1):
+        if int(i**0.5)==i**0.5:
+            answer -= i
+        else:
+            answer += i
+    return answer
+
+left = 24
+right = 27
+
+print(solution(left,right))
