c7123521ab25390990e08172532b7361b50427ce
Dongmin <kdm3578@gmail.com>
2022-03-26 09:42:21 +0900

    인프런 인강 update

--- 인프런 파이썬 강의/섹션5-스택,큐,해쉬,힙/가장 큰 수.py
+++ 인프런 파이썬 강의/섹션5-스택,큐,해쉬,힙/가장 큰 수.py
@@ -1,6 +1,6 @@
 #---------- 정답 입력 코드 -----------------
 import sys
-NUM = 1
+NUM = 3
 sys.stdin=open(f"/Users/gimdongmin/Desktop/파이썬 알고리즘 강의/섹션 5/1. 가장 큰 수/in{NUM}.txt","rt")
 file = open(f"/Users/gimdongmin/Desktop/파이썬 알고리즘 강의/섹션 5/1. 가장 큰 수/out{NUM}.txt")
 answer = file.read()
@@ -8,14 +8,49 @@ print(f"answer : \n{answer}")
 print(f"{'-'*50}")
 #----------------------------------------
 
-#------------ 내 풀이 코드 -----------------
+#------------ 내 풀이 코드 -----------------  => 설명 들어도 실패
+
 def solution():
+    n,m = input().split()
+    M = int(m)
+    q = []
+    cnt = 0
+    for i in n:
+        if M <=0:
+            q.append(i)
+            continue
+        if len(q)==0:
+            q.append(i)
+            continue
+        for j in q:
+            if int(j) < int(i):
+                cnt+=1
+                M-=1
 
-    res = 0
-    return res
+        for _ in range(cnt):
+            q.pop()
+        q.append(i)
+        cnt=0
+    return ''.join(q[:len(n)-int(m)])
 
 
-print(solution())
+
+
+
+
+# print(solutionlution())
 
 
 #-----------영상 풀이 코드 -------------------
+num,m =map(int, input().split())
+num = list(map(int,str(num)))
+stack = []
+for x in num:
+    while stack and m>0 and stack[-1]<x:
+        stack.pop()
+        m-=1
+    stack.append(x)
+if m!=0:
+    stack = stack[:-m]
+res = ''.join(map(str,stack))
+print(res)
---
+++ 인프런 파이썬 강의/섹션5-스택,큐,해쉬,힙/쇠막대기.py
@@ -0,0 +1,26 @@
+#---------- 정답 입력 코드 -----------------
+import sys
+NUM = 3
+sys.stdin=open(f"/Users/gimdongmin/Desktop/파이썬 알고리즘 강의/섹션 5/2. 쇠막대기/in{NUM}.txt","rt")
+file = open(f"/Users/gimdongmin/Desktop/파이썬 알고리즘 강의/섹션 5/2. 쇠막대기/out{NUM}.txt")
+answer = file.read()
+print(f"answer : \n{answer}")
+print(f"{'-'*50}")
+#----------------------------------------
+
+#------------ 내 풀이 코드 -----------------  => 설명 들어도 실패
+
+def solution():
+    k = input()
+
+
+
+
+
+
+
+print(solution())
+
+
+#-----------영상 풀이 코드 -------------------
+
